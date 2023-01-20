b2eb15c2f0befcfeb47dd1c1d6c7af03ed5c1ff2
Lazy Raccoon <counter088@gmail.com>
2023-01-20 17:59:08 +0200

    Modified the home template.

--- core/settings/dev.py
+++ core/settings/dev.py
@@ -1,7 +1,7 @@
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-)nh2!0xt3=_dg#f7)n1&49!)47&lnt6@=_2z0a5bs3ggcd5z3_"
